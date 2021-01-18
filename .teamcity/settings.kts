99546ad5a59adb2e7e6c28a09223b260bcc91544
zjie <jie.zhang@leica-geosystems.com.cn>
2021-01-18 14:12:45 +0800

    Remove TeamCity related scripts to internal Repo.

--- .teamcity/settings.kts
+++
@@ -1,4 +0,0 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-
-version = "2020.1"
-project(Project)
--- build-xerces-win32-2008-release.bat
+++
@@ -1,9 +0,0 @@
-set BUILD_DIR=%~n0
-set INSTALL_DIR=xerces-install-win32-2008-Release
-set cleanBuild=%1
-if "%cleanBuild%"=="1" rd /Q /S %BUILD_DIR%
-
-mkdir %BUILD_DIR% 
-call cmake -B %BUILD_DIR% -S %~dp0 -G "Visual Studio 9 2008" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR%
-call cmake --build %BUILD_DIR% --config Release
-call cmake --build %BUILD_DIR% --config Release --target install
